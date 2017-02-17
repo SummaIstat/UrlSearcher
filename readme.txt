d16047a7c8724fcb71a0b1cc0e9f67fcea77b688
summa <donato.summa@istat.it>
2017-02-17 12:16:56 +0100

    First commit of the project

---
+++ readme.txt
@@ -0,0 +1,86 @@
+**************************************
+*****     UrlSearcher readme     *****
+**************************************
+
+======================================================================
+What is UrlSearcher and what is it intended to be used for
+======================================================================
+
+In a nutshell UrlSearcher is a Java application that takes as input a list of names and 
+for each of them queries a search engine and write a txt file on disk containing the first 
+10 urls provided by the search engine as response to the query.
+
+I developed and used this program in order to acquire for an enterprise name a list of websites, 
+the assumption is that: if an enterprise has an official website, this must be found within the 
+first 10 result of a search engine. It will be up to us to decide which one of the collected 
+website is the official one for that particular enterprise.
+
+======================================================================
+How is the project folder made
+======================================================================
+
+The RootJuice folder is an Eclipse project ready to be run or modified in the IDE (you just have to import the project "as an existing project" and optionally change some configuration parameters in the code).
+
+Ignoring the hidden directories and the hidden files, under the main directory you can find 4 subdirectories :
+5) src => contains the source code of the program
+1) bin => contains the compiled version of the source file
+3) lib => contains the jar file (libraries) that the program needs
+4) sandbox => contains the executable jar file that you have to use in order to launch the program, the configuration file that you have to pass as parameter and some test input files that you can modify on the basis of your needs
+
+As you probably already know it is definitely not a good practice to put all this stuff into a downloadable project folder, but i decided to break the rules in order to facilitate your job. Having all the stuff that will be necessary in just one location and by following the instructions you should be able to test the whole environment in 5-10 minutes.
+
+======================================================================
+How to execute the program on your PC by using the executable jar file
+======================================================================
+
+If you have Java already installed on your PC you just have to apply the following instruction points:
+
+1) create a folder on your filesystem (let's say "myDir")
+
+2) copy the content of the sandbox directory into "myDir"
+
+3) customize the parameters inside the urlSearcherConf.properties file :
+        
+        If you are behind a proxy simply uncomment and customize the 2 parameters under the "proxy section" by removing the initial # character
+        
+        Change the value of the parameters under the "paths section" according with the position of the files and folders on your filesystem.
+
+4) open a terminal, go into the myDir directory, type and execute the following command:
+        java -jar UrlSearcher.jar urlSearcherConf.properties
+
+5) at the end of execution you should find 9 txt files inside the directory myDir/txtFiles and a file called seed_[dateTime].txt in the myDir directory
+
+======================================================================
+LINUX			
+======================================================================
+
+If you are using a Linux based operating system open a terminal and type on a single line :
+
+java -jar 
+-Xmx_AmountOfRamMemoryInMB_m
+/path_of_the_directory_containing_the_jar/UrlSearcher.jar 
+/path_of_the_directory_containing_the_conf_file/urlSearcherConf.properties
+
+eg:
+
+java -jar -Xmx1024m UrlSearcher.jar urlSearcherConf.properties
+
+java -jar -Xmx1024m /home/summa/workspace/UrlSearcher/sandbox/UrlSearcher.jar /home/summa/workspace/UrlSearcher/sandbox/urlSearcherConf.properties
+
+
+======================================================================
+WINDOWS			
+======================================================================
+
+If you are using a Windows based operating system you just have to do exactly the same, the only difference is that you have to substitute the slashes "/" with the backslashes "\" in the filepaths.
+
+eg:
+
+java -jar -Xmx1536m C:\workspace2\UrlSearcher\sandbox\UrlSearcher.jar C:\workspace2\UrlSearcher\sandbox\urlSearcherConf.properties
+
+
+======================================================================
+Considerations
+======================================================================
+
+This program is still a work in progress so be patient if it is not completely fault tolerant; in any case feel free to contact me (donato.summa@istat.it) if you have any questions or comments.
---
+++ src/it/istat/urlsearcher/UrlSearcher.java
@@ -0,0 +1,340 @@
+package it.istat.urlsearcher;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.Properties;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+//*******************************
+//***** Author				*****
+//***** Donato Summa		*****
+//*******************************
+
+public class UrlSearcher {
+
+    //private static final String URLEncoder = null;
+	List<String> firmNamesList = new ArrayList<String>();
+    List<String> firmIdsList = new ArrayList<String>();
+    
+    // program parameters
+    //private static String proxyHost = "proxy.istat.it";
+    //private static String proxyPort = "3128";
+    //private static String firmsNamesFilePath = "/home/summa/workspace/ICTScraper/sandbox/firmNamesTrain.txt";
+    //private static String firmsIdsFilePath = "/home/summa/workspace/ICTScraper/sandbox/firmIdsTrain.txt";
+    //private static String txtFilesFolderPath = "/home/summa/workspace/ICTScraper/sandbox/txtFiles";
+    //private static String seedFileFolderPath = "/home/summa/workspace/ICTScraper/sandbox/jCrawlerSeedFile";
+    private static String proxyHost = null;
+    private static String proxyPort = null;
+    private static String firmsNamesFilePath;
+    private static String firmsIdsFilePath;
+    private static String txtFilesFolderPath;
+    private static String seedFileFolderPath;
+    private static String seedFileName = "seed";
+
+    
+    // if you are under Windows OS you have to set something like this
+    //private static String firmsNamesFilePath = "C:\\myFolder\\myFileWithTheListOfEnterpriseNames.txt";
+    //private static String firmsIdsFilePath = "C:\\myFolder\\myFileWithTheListOfEnterpriseIds.txt";
+    //private static String txtFilesFolderPath = "C:\\myFolder\\myResultsFolder";
+    
+    // if you are under Linux OS you have to set something like this
+    //private static String firmsNamesFilePath = "/home/myHome/myFolder/myFileWithTheListOfEnterpriseNames.txt";
+    //private static String firmsIdsFilePath = "/home/myHome/myFolder/myFileWithTheListOfEnterpriseIds.txt";
+    //private static String txtFilesFolderPath = "/home/myHome/myFolder/myResultsFolder";
+    
+    public static void main(String[] args) throws IOException {
+             
+        //=====================================================================================================
+    	DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+        Date startDateTime = new Date();
+        System.out.println("Starting datetime = " + dateFormat.format(startDateTime)); //15/12/2014 15:59:48
+    	//=====================================================================================================
+        
+        UrlSearcher scraper = new UrlSearcher();
+        scraper.configure(args);
+        scraper.scrape(firmsNamesFilePath, firmsIdsFilePath, txtFilesFolderPath);
+        scraper.generateUrlCrawlerSeedFile();
+        
+        //=====================================================================================================
+      	Date endDateTime = new Date();
+      	System.out.println("Started at = " + dateFormat.format(startDateTime)); //15/12/2014 15:59:48
+      	System.out.println("Ending datetime = " + dateFormat.format(endDateTime)); //15/12/2014 15:59:48
+      	//=====================================================================================================
+    }
+    
+    private void configure(String[] args) throws IOException {
+    	
+    	if (args.length == 1){
+			if (isAValidFile(args[0])){
+				FileInputStream fis = new FileInputStream(args[0]);
+				InputStream inputStream = fis;
+				Properties props = new Properties();
+				props.load(inputStream);
+				
+				// if you are behind a proxy you have to set the IP address and the port number of the proxy
+				// PROXY_HOST
+				if(props.getProperty("PROXY_HOST") != null){
+					proxyHost = props.getProperty("PROXY_HOST");
+					System.getProperties().put("http.proxyHost", proxyHost);
+				}
+				
+				// PROXY_PORT
+				if(props.getProperty("PROXY_PORT") != null){
+					proxyPort = props.getProperty("PROXY_PORT");
+					System.getProperties().put("http.proxyPort", proxyPort);
+				}
+				
+				// Mandatory parameters
+				// FIRM_NAMES_FILE_PATH
+				if(props.getProperty("FIRM_NAMES_FILE_PATH") != null){
+					firmsNamesFilePath = props.getProperty("FIRM_NAMES_FILE_PATH");
+				}else{
+					System.out.println("Wrong/No configuration for the parameter FIRM_NAMES_FILE_PATH !");
+					System.exit(1);
+				}
+				
+				// FIRM_IDS_FILE_PATH
+				if(props.getProperty("FIRM_IDS_FILE_PATH") != null){
+					firmsIdsFilePath = props.getProperty("FIRM_IDS_FILE_PATH");
+				}else{
+					System.out.println("Wrong/No configuration for the parameter FIRM_IDS_FILE_PATH !");
+					System.exit(1);
+				}
+				
+				// TXT_FILES_FOLDER_PATH
+				if(props.getProperty("TXT_FILES_FOLDER_PATH") != null){
+					txtFilesFolderPath = props.getProperty("TXT_FILES_FOLDER_PATH");
+					if (!isAValidDirectory(txtFilesFolderPath)){
+			        	System.out.println("The TXT_FILES_FOLDER_PATH parameter that you set ( " + txtFilesFolderPath + " ) is not valid");
+			        	System.exit(1);
+			        }
+				}else{
+					System.out.println("Wrong/No configuration for the parameter TXT_FILES_FOLDER_PATH !");
+					System.exit(1);
+				}
+				
+				// SEED_FILE_FOLDER_PATH
+				if(props.getProperty("SEED_FILE_FOLDER_PATH") != null){
+					seedFileFolderPath = props.getProperty("SEED_FILE_FOLDER_PATH");
+					if (!isAValidDirectory(seedFileFolderPath)){
+			        	System.out.println("The SEED_FILE_FOLDER_PATH parameter that you set ( " + seedFileFolderPath + " ) is not valid");
+			        	System.exit(1);
+			        }
+				}else{
+					System.out.println("Wrong/No configuration for the parameter SEED_FILE_FOLDER_PATH !");
+					System.exit(1);
+				}
+				
+			} else {
+				System.out.println("Error opening file " + args[0] + " or non-existent file");
+				System.out.println("==>  program execution terminated <==");
+				System.exit(1);
+			}
+		} else {
+			System.out.println("usage: java -jar UrlSearcher.jar [urlSearcherConf.properties fullpath]");
+			System.exit(1);
+		}			
+	}
+
+	private int scrape(String firmsNamesFilePath, String firmsIdsFilePath, String txtFilesFolderPath) {
+        
+        firmNamesList = UrlSearcher.getListFromFile(firmsNamesFilePath);
+        firmIdsList = UrlSearcher.getListFromFile(firmsIdsFilePath);
+        File resultsFolder = new File(txtFilesFolderPath);
+        
+        for(int i=0 ; i < firmNamesList.size() ; i++){ 
+            String firmName = firmNamesList.get(i);
+            String firmId = firmIdsList.get(i);
+            System.out.println((i+1) + " / " + firmNamesList.size() + " ) " + "I am processing " + firmName + " having ID " + firmId );
+            String path = resultsFolder.getPath() + File.separator + firmId + ".txt";
+            
+            try {
+                File file = new File(path);
+                FileWriter fw = new FileWriter(file);
+                BufferedWriter bw = new BufferedWriter(fw);
+
+                bw.write("ID = " + firmId);
+                bw.newLine();
+                bw.write("Name = " + firmName);
+                bw.newLine();
+                bw.newLine();
+                
+                StringBuffer sb = bingSearch(firmName);
+                
+                Document doc = Jsoup.parse(sb.toString());
+                Elements resultLinks = doc.select("li[class=b_algo] h2 a");
+                
+                for(int j=0 ; j < resultLinks.size() ; j++){ 
+                    bw.newLine();
+                    bw.newLine();
+                    //bw.write(sb.toString());
+                    
+                    Element element;            
+                    element = resultLinks.get(j);
+                    bw.write(element.text().toString());
+                    bw.newLine();
+                    bw.write(URLDecoder.decode(element.attr("href"),"UTF-8")); 
+                    //bw.write(element.attr("href")); // old version
+                    bw.newLine();
+                    bw.newLine();
+                }
+                
+                bw.flush();
+                bw.close();
+            }
+            catch(IOException e) {
+              e.printStackTrace();
+            }
+        } 
+        
+        return 0;
+    }
+        
+    private  StringBuffer bingSearch(String searchTerm){
+        StringBuffer totalResults = new StringBuffer();
+        URL urlObj;
+        HttpURLConnection connection;
+        String userAgent = "(Mozilla/5.0 (Windows; U; Windows NT 6.0;en-US; rv:1.9.2)" + " Gecko/20100115 Firefox/3.6)";
+        //int responseCode;
+        
+        try {
+            searchTerm = searchTerm.replace(' ','+');
+            urlObj = new URL("http://www.bing.com/" + "search?q=" + URLEncoder.encode(searchTerm, "UTF-8"));
+            connection = (HttpURLConnection) urlObj.openConnection();
+            connection.setRequestMethod("GET");
+            connection.setRequestProperty("User-Agent", userAgent);
+                        
+            //responseCode = connection.getResponseCode();
+            BufferedReader in;
+            in = new BufferedReader(new InputStreamReader(connection.getInputStream(),StandardCharsets.UTF_8));
+            totalResults = new StringBuffer();
+            String inputLine;
+            while ((inputLine = in.readLine()) != null){
+                totalResults.append(inputLine);
+            }
+            in.close();  
+            //System.out.println(totalResults);
+            
+        } catch (IOException ex) {
+            System.err.println(ex.getMessage());
+        } finally {
+            return totalResults;
+        }
+        
+    }
+    
+    private void generateUrlCrawlerSeedFile() {
+    	// every row in the seed file will have the following structure :
+    	// pageUrl	firmId	linkPosition
+    	
+		Date dateTime = Calendar.getInstance().getTime();
+        DateFormat formatter = new SimpleDateFormat("yyyyMMddHHmmss");
+        String now = formatter.format(dateTime);
+        String path = seedFileFolderPath + File.separator + seedFileName + "_" + now + ".txt";
+        
+		try {
+			
+			File file = new File(path);
+			FileWriter fw = new FileWriter(file);
+			BufferedWriter bw = new BufferedWriter(fw);
+			
+			String txtFilesFolder = txtFilesFolderPath;			
+			File folder = new File(txtFilesFolder);
+			
+		    for (final File fileEntry : folder.listFiles()) {
+		        if (fileEntry.isDirectory()) {
+		            // do nothing
+		        } else {
+		        	System.out.println("Analysis of the file " + fileEntry.getName());
+		            FileInputStream fis = new FileInputStream(fileEntry);
+					InputStream is = fis;
+					BufferedReader br = new BufferedReader(new InputStreamReader(is));
+					String strLine;
+					String codiceAzienda = ""; // codiceAzienda means firmId
+					int codiceLink = 1; // codiceLink means the position of the link in the links list provided by the search engine
+					while ((strLine = br.readLine()) != null) {
+						codiceAzienda = fileEntry.getName().substring(0, fileEntry.getName().length() - 4);
+						if (strLine.startsWith("http")){
+							//bw.write(strLine.toLowerCase() + "\t" + "codiceAzienda=" + codiceAzienda + "\t" +"codiceLink=" + codiceLink);
+							bw.write(strLine.toLowerCase() + "\t" + codiceAzienda + "\t" + codiceLink);
+							bw.newLine();
+							codiceLink++;
+						}
+						
+					}					
+					is.close();
+		        }
+		    }
+		    
+		    bw.close();
+		    
+		}catch (Exception e) {
+			e.printStackTrace();
+			System.err.println("Error: " + e.getMessage());
+		}
+    }
+    
+    //=====================================================================================================
+    //==========		Utility Methods
+    //=====================================================================================================
+    
+    public static boolean isAValidFile(String filePathString) {
+		File f = new File(filePathString);
+		if(f.exists() && !f.isDirectory()) { 
+			return true;
+		}
+		return false;
+	}
+    
+    public static boolean isAValidDirectory(String dirPathString) {
+		File f = new File(dirPathString);
+		if(f.exists() && f.isDirectory()) { 
+			return true;
+		}
+		return false;
+	}
+    
+    public static List<String> getListFromFile(String filePath) {
+        List<String> orderedList = new ArrayList<String>();
+        File file = new File(filePath);
+        FileInputStream fis = null;
+        try {
+                //InputStream is = Main.class.getResourceAsStream(fileConListaNomiAziendePath);
+                fis = new FileInputStream(file);
+                //BufferedReader br = new BufferedReader(new InputStreamReader(is));
+                BufferedReader br = new BufferedReader(new InputStreamReader(fis));
+                String strLine;
+                while ((strLine = br.readLine()) != null) {
+                        orderedList.add(strLine);
+                }
+                fis.close();
+        } catch (Exception e) {
+                e.printStackTrace();
+                System.err.println("Error: " + e.getMessage());
+                System.exit(1);
+        }
+        return orderedList;
+    }
+    
+}
